cf0529915b71ce89bbd620e591af1f9d96433c21
Bryan F Rodrigues <93285267+BryanFRodrigues@users.noreply.github.com>
2025-03-07 01:10:19 +0000

    Primeiro commit

---
+++ Aula1/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do professor: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite a quantidade de anos de experiência: ");
+        int anosExperiencia = sc.nextInt();
+        Professor p = new Professor(nome, anosExperiencia);
+        System.out.println(p.toString());
+        System.out.println("Classificação: " + p.classificarProfessor());
+    }
+}
---
+++ Aula1/src/Professor.java
@@ -0,0 +1,34 @@
+public class Professor {
+    private String nome;
+    private int anosExperiencia;
+
+    public Professor(String nome, int anosExperiencia) {
+        this.nome = nome;
+        this.anosExperiencia = anosExperiencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnosExperiencia() {
+        return anosExperiencia;
+    }
+
+
+
+    public String classificarProfessor() {
+        return switch (anosExperiencia) {
+            case 0, 1, 2 -> "Iniciante";
+            case 3, 4, 5 -> "Intermediário";
+            case 6, 7, 8 -> "Avançado";
+            default -> "Mestre";
+        };
+    }
+
+        @Override
+        public String toString(){
+            return "Professor: " + nome + ", " + anosExperiencia + " anos de experiência";
+        }
+    }
+
